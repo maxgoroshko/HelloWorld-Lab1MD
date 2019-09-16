f6a140b8cf296971a3fa4f8637406778812f5bb1
MaxGoroshkoTU <tug47632@temple.edu>
2019-09-16 16:12:16 -0400

    clicking a button changes a text

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "You clicked main button", Toast.LENGTH_LONG).show();
+                ((TextView)findViewById(R.id.textView)).setText("Your Text");
+            }
+        });
     }
 }
